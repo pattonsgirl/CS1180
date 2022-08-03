24f256eb8b1a01b51a0b82fc58fa8f7f9d37f0c7
pattonsgirl <pattonsgirl@gmail.com>
2022-08-03 15:34:32 -0400

    Prints initals in asterisks for fun

---
+++ 01-Intro/initials.java
@@ -0,0 +1,13 @@
+public class initials {
+    public static void main(String[] args) {
+        System.out.println("**        ***    **********    **********      ");
+        System.out.println("**      ***      **            **        **    ");
+        System.out.println("**    ***        **            **         **   ");
+        System.out.println("**  ***          **            **          **  ");
+        System.out.println("*****            ********      **          **  ");
+        System.out.println("**  ***          **            **          **  ");
+        System.out.println("**    ***        **            **         **   ");
+        System.out.println("**      ***      **            **        **    ");
+        System.out.println("**        ***    **********    **********      ");
+    }
+}
