854e8852f4392d5614c5406caa0a5d998e735dd6
pattonsgirl <pattonsgirl@gmail.com>
2022-08-03 15:36:58 -0400

    writing javadoc

--- 01-Intro/initials.java
+++ 01-Intro/initials.java
@@ -1,3 +1,13 @@
+/******************************************************************************
+ * Compilation: javac initials.java
+ * Execution: java initials
+ *
+ * Prints the initials K E D
+ * Idea from https://introcs.cs.princeton.edu/java/11hello/Initials.java.html
+ *
+ * 
+ ******************************************************************************/
+
 public class initials {
     public static void main(String[] args) {
         System.out.println("**        ***    **********    **********      ");
