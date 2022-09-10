bcf2c2496f2f07bf402968d561572a56fe371197
pattonsgirl <pattonsgirl@gmail.com>
2022-09-10 12:46:21 -0400

    cleaning up guessing game

--- 03-Conditionals/guessinggame.java
+++ 03-Conditionals/guessinggame.java
@@ -4,33 +4,29 @@ import java.util.Scanner;
 public class guessinggame {
     public static void main(String[] args) {
         // Generate a random number between 0 and 10 (inclusive)
-        // Generate a random number between 0 and 10 (inclusive)
+        // ggpseudocode.java created a series of TODOs to complete
+        // psuedocode is when we outline steps that are almost the final product
 
         Random randGen = new Random();
-
-        // create a random number object
         // generate a random number in range 0-10
         int randNum = randGen.nextInt(11);
 
         // Get a guess from the user
-
-        // scan userinput
         Scanner scnr = new Scanner(System.in);
+        System.out.println("Give a number between 0 and 10: ");
         int userinput = scnr.nextInt();
+
+        // The below print statement is left for debugging purposes to check
+        // that the relational conditions are working correctly.
+        // System.out.println("The computer picked " + randNum);
 
         // Check if guess is equal to the random number
         // If equal, print “You win!”, else print “No luck, number was ____”
-        // TODO: if userinput == randNum
-        System.out.println("The computer picked " + randNum);
         if (userinput == randNum) {
             System.out.println("You win");
         } else {
             System.out.println("You lose.  Number was " + randNum);
         }
-        // TODO: println("You win!");
-        // TODO: else
-        // TODO: println("You lose. Number was: " + randNum);
-
         // Challenge: hot or cold?
         // If user guess is +2 or -2 digits from random number, print “You were almost
         // right!”
