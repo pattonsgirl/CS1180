1a3fc2696692ecd4fc78e0c70daefac53891055c
pattonsgirl <pattonsgirl@gmail.com>
2022-09-12 12:45:55 -0400

    code templates for week

---
+++ 04-Loops/dowhilesamples.java
@@ -0,0 +1,10 @@
+
+public class dowhilesamples {
+    public static void main(String[] args) {
+        /*
+         * do {
+         * do these things, then evaluate condition
+         * } while (expression is true);
+         */
+    }
+}
