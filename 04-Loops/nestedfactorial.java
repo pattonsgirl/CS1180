7643db03d7feff58854da048e39dfa8fdd6038ee
pattonsgirl <pattonsgirl@gmail.com>
2022-09-21 13:18:12 -0400

    var name change

--- 04-Loops/nestedfactorial.java
+++ 04-Loops/nestedfactorial.java
@@ -2,7 +2,7 @@ public class nestedfactorial {
     public static void main(String[] args) {
         // factorials with nested loops
         int howMany = 10;
-        int factorialresult = 1;
+        int factresult = 1;
         // sample of expected results as loop iterates
         // 1! = 1
         // 2! = 2
@@ -13,15 +13,15 @@ public class nestedfactorial {
             System.out.print("factorial " + i + "!: ");
             for (int j = 1; j <= i; j++) {
                 // System.out.print(j);
-                factorialresult = factorialresult * j;
+                factresult = factresult * j;
                 // I got in my head, wrong answer below
                 // factorialresult += factorialresult * j;
             }
-            System.out.print(factorialresult);
+            System.out.print(factresult);
             // System.out.print(" factorial of " + i + " is: " + factorialresult);
             System.out.print("\n");
             // reset factorial result at end of each iteration
-            factorialresult = 1;
+            factresult = 1;
         }
     }
 }
