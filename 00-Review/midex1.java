cb6315c6d2e7405205d214203456fb319c18463f
pattonsgirl <pattonsgirl@gmail.com>
2022-10-03 13:27:55 -0400

    midterm exercises

---
+++ 00-Review/midex1.java
@@ -0,0 +1,18 @@
+/*
+ * Given a string
+ * return a string where for every char in the original there are two chars.
+ * Samples:
+ * Input: The
+ * Output: TThhee
+ * Input: Aabb
+ * Output: AAAAbbbb
+ * Input: Hi-There
+ * Output: HHii--TThheerree
+
+ */
+
+public class midex1 {
+    public static void main(String[] args) {
+
+    }
+}
