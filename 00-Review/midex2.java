aa4c5e7b640529bd607b57110f16702f3f1a18e9
pattonsgirl <pattonsgirl@gmail.com>
2022-10-03 13:28:02 -0400

    ex 2

---
+++ 00-Review/midex2.java
@@ -0,0 +1,28 @@
+/*
+ * Given a day of the week encoded as 
+ * 0=Sun, 1=Mon, 2=Tue, ...6=Sat, and a boolean indicating 
+ * if we are on vacation, return a string of the form "7:00" 
+ * indicating when the alarm clock should ring.
+ * Weekdays, the alarm should be "7:00" 
+ * On the weekend it should be "10:00". 
+ * If we are on vacation -- then on weekdays it should be "10:00" 
+ * and weekends it should be "off".
+ * Samples:
+ * Input: 1 false
+ * Output:  “Set alarm to 7:00 AM"
+ * Input: 5 false 
+ * Output:  “Set alarm to 7:00 AM"
+ * Input: 0 false 
+ * Output:  “Set alarm to 10:00 AM”
+ * Input: 3 true
+ * Output: “Set alarm to 10:00 AM”
+ * Input: 0 true
+ * Output: “Set alarm to off”
+
+ */
+
+public class midex2 {
+    public static void main(String[] args) {
+
+    }
+}
