e354281b23bf35f5e66e67da412a2bc695a06220
pattonsgirl <pattonsgirl@gmail.com>
2022-10-12 15:25:06 -0400

    demo code goals for 10/14

---
+++ 07-Arrays/arraydemos.java
@@ -0,0 +1,13 @@
+public class arraydemos {
+    public static void main(String[] args) {
+        /*
+         * Read user input into an array of 5 integers
+         * Print all elements of the array from 0 to 4
+         * Print all elements of the array from 4 to 0
+         * Find minimum value
+         * Find maximum value
+         * Bonus: Find average of values
+         * 
+         */
+    }
+}
