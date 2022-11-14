cd8e6fcc674e86ef81fcc0aded4e7fbc32542495
pattonsgirl <pattonsgirl@gmail.com>
2022-11-14 17:10:07 -0500

    class code 11/14

--- 11-Classes-Objects/HashDemo/src/App.java
+++ 11-Classes-Objects/HashDemo/src/App.java
@@ -1,5 +1,63 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        // memory jog on HashMaps -
+        // https://github.com/pattonsgirl/CS1180/tree/main/09-HashMaps
         System.out.println("Hello, World!");
+        Book booky = new Book();
+        Book mybook = new Book("Book Title", 1998);
+        // proving that println uses toString method declared in Object class (if it
+        // exists)
+        System.out.println(booky);
+        System.out.println(mybook);
+
+        // goofing off with generating hashCodes of Strings
+        // if two object generate the SAME hashCode, then the object ARE THE SAME
+        // a hashCode is generated from an object
+        // the hashCode CANNOT recreate the object - calculation is one way
+        // checkout hasCode return value here:
+        // https://www.programiz.com/java-programming/library/string/hashcode
+        String str1 = "George";
+        String str2 = "George of the Jungle";
+        String str3 = "George";
+
+        System.out.println("hashCode of " + str1 + " is: " + str1.hashCode());
+        System.out.println("hashCode of " + str2 + " is: " + str2.hashCode());
+        System.out.println("hashCode of " + str3 + " is: " + str3.hashCode());
+
+        // Using a HashMap to track who has checked out a given book
+        // This assumes there is only ONE copy of a given book
+        // Otherwise a Book would NOT make a good KEY
+        // The String name of a person who checked out the book is the VALUE
+        // associated with the key
+        HashMap<Book, String> checkout = new HashMap<>();
+
+        checkout.put(mybook, "Janet");
+        Book cheese1 = new Book("Cheese", 2001);
+        Book cheese2 = new Book("Cheese", 1984);
+        checkout.put(cheese2, "Bob");
+        checkout.put(cheese1, "Sam");
+        checkout.put(new Book("1984", 1949), "George");
+        checkout.put(cheese1, "Billy");
+
+        // I added a print statement to my hashCode generator for Book objects
+        /*
+         * System.out.println(booky.hashCode());
+         * System.out.println(cheese1.hashCode());
+         * System.out.println(cheese2.hashCode());
+         */
+        // String myCode = Integer.toString(cheese2.hashCode());
+        System.out.println(checkout.get(cheese1));
+        System.out.println(checkout.get(new Book("Cheese", 2001)));
+        System.out.println(checkout.get(new Book("1984", 1949)));
+
+        // what if you wanted to have users be keys, and the Books (multiple)
+        // they check out be the value?
+        HashMap<String, ArrayList<Book>> usercheckout = new HashMap<>();
+        // TODO: if I choose this route, the hashCode will generate from
+        // String.hashCode (not from Book)
+
     }
 }
--- 11-Classes-Objects/HashDemo/src/Book.java
+++ 11-Classes-Objects/HashDemo/src/Book.java
@@ -4,7 +4,7 @@ public class Book {
     private String content;
 
     public Book() {
-        this.name = "";
+        this.name = "NotSet";
         this.published = 1565;
         this.content = "...";
     }
@@ -12,6 +12,7 @@ public class Book {
     public Book(String name, int published) {
         this.name = name;
         this.published = published;
+        this.content = "...";
     }
 
     public String getName() {
@@ -31,14 +32,63 @@ public class Book {
     }
 
     public String toString() {
-        return this.name + " " + this.published + " " + this.content;
+        return this.name + " " + this.published + " \n" + this.content;
     }
 
+    // hashMaps use hashCodes when keys are added.
+    // to generate a unique code for our key, which is an object,
+    // we define the properties that generate the value
+    @Override
     public int hashCode() {
-        return 0;
+        // if book name is null, base hashcode on year published
+        if (this.name == null) {
+            return this.published;
+        }
+
+        // else return (hash of name + year published)
+        System.out.println("Unique hash is: " + this.name.hashCode() + " + " + this.published + " = " + (this.published
+                + this.name.hashCode()));
+        return this.published + this.name.hashCode();
     }
 
-    public boolean equals() {
+    // a proper equals - why?
+    // for use by .contains and hashMap methods, like get
+    @Override
+    public boolean equals(Object b) {
+        // if the variables are located in the same place, they're the same
+        /*
+         * This could be set up by
+         * Book myBook = new Book();
+         * Book myOtherBook = myBook;
+         */
+        if (this == b) {
+            // System.out.println("Clones");
+            return true;
+        }
+
+        // if comparedObject is not of type Book, the objects aren't the same
+        if (!(b instanceof Book)) {
+            // System.out.println("This isn't even a book!");
+            return false;
+        }
+
+        // convert the object to a Book-object
+        Book comparedBook = (Book) b;
+
+        // if the instance variables of the objects are the same, so are the objects
+        if (this.name.equals(comparedBook.name) &&
+                this.published == comparedBook.published) { // && this.content.equals(comparedBook.content)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // this one is not a real override of .equals... see the good one above
+    public boolean equals(Book b) {
+        if (this.name.equals(b.name) && this.published == b.published) {
+            return true;
+        }
         return false;
     }
 }
