8240bc2c389b8fb707e26ba544977a95a36ead8f
pattonsgirl <pattonsgirl@gmail.com>
2022-11-28 13:18:14 -0500

    class code 11/28

--- 12-Class-Project/src/Account.java
+++ 12-Class-Project/src/Account.java
@@ -1,23 +1,150 @@
-public class Account {
+
+/**
+ * Class to create objects of Accounts.
+ * <p>
+ * Account objects include a unique account number, an account owner, 
+ * account type, and funds 
+ * 
+ * @author Kayleigh Duncan
+ * @version 1.0
+ */
+
+import java.util.Random;
+import java.lang.Comparable;
+
+public class Account implements Comparable {
+    // public class Account implements Comparable<Account>{
+
     private Integer accountNumber;
     private String accountOwner;
     private String accountType;
     private Double money;
 
+    /**
+     * Class constructor if no account details are given when creating
+     * the account.
+     * <p>
+     * Minimum of accountNumber is generated from dummy String
+     * <p>
+     * Object's properties can then be modified using setter methods
+     */
     public Account() {
         // there is no point in having an account with no info
-        // maybe prompts for account?
+        // could write set of prompts here to set object's values
+        this.accountNumber = generateAccountNumber("abcdefghijklmnopqrstuvwxyz");
     }
 
+    /**
+     * Class constructor when owner, account type, and initial funds are known
+     * on object creation
+     * <p>
+     * generates an account number based on owner name
+     */
     public Account(String owner, String type, Double money) {
         this.accountOwner = owner;
         this.accountType = type;
         this.money = money;
         // create unique account number
-        this.accountNumber = genAccountNumber(owner);
+        this.accountNumber = generateAccountNumber(owner);
     }
 
-    private Integer genAccountNumber(String s) {
+    /**
+     * Returns unique account number hashed from string + random int
+     * <p>
+     * Private because accountNumbers should not be something modified
+     * on a whim - this method is only accessible by methods in this class
+     * 
+     * @param s string to generate hashCode
+     * @return Integer accountNumber
+     */
+    private Integer generateAccountNumber(String s) {
+        Random randGen = new Random();
+        Integer accountNum = s.hashCode() + randGen.nextInt(2000);
+        System.out.println("Generated account number " + accountNum);
+        return accountNum;
+    }
+
+    /**
+     * Returns account number
+     * 
+     * @return Integer
+     */
+    public Integer getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    /**
+     * Sets new account owner for account object
+     * 
+     * @param newowner String new account owner
+     */
+    public void setAccountOwner(String newowner) {
+        this.accountOwner = newowner;
+    }
+
+    /**
+     * Returns account owner
+     * 
+     * @return String
+     */
+    public String getAccountOwner() {
+        return this.accountOwner;
+    }
+
+    /**
+     * Returns funds in account
+     * 
+     * @return Double money in account
+     */
+    public Double getAccountFunds() {
+        return this.money;
+    }
+
+    /**
+     * Sets initial account funds
+     * 
+     * @param money inital funds for account
+     */
+    public void setAccountFunds(Double money) {
+        this.money = money;
+    }
+
+    /**
+     * sets account type (Checking, Savings, Credit)
+     * 
+     * @param type account type
+     */
+    public void setAccountType(String type) {
+        this.accountType = type;
+    }
+
+    /**
+     * Returns formatted String for objects of type Account
+     * <p>
+     * Formatting in csv - comma separated value - syntax
+     * <p>
+     * Note: Override is optional here
+     * 
+     * @return String formatted data
+     */
+    @Override
+    public String toString() {
+        return this.accountNumber + ", " + this.accountOwner + ", " + this.accountType + ", " + this.money;
+    }
+
+    // TODO: needs to implement what?
+    // This defines how our account objects are compared for sorting
+    /*
+     * @Override
+     * public int compareTo(Account a) {
+     * return 0;
+     * }
+     */
+
+    @Override
+    public int compareTo(Object o) {
+        // TODO Auto-generated method stub
+        // is it an account object?
         return 0;
     }
 }
--- 12-Class-Project/src/App.java
+++ 12-Class-Project/src/App.java
@@ -1,6 +1,16 @@
+
+/**
+ * Driver for Banking application
+ * @author Kayleigh Duncan
+ * @version 1.0
+ */
+
 import java.util.*;
 
 public class App {
+    /*
+     * Prints option menu to screen
+     */
     public static void printMenu() {
         System.out.println("Read carefully because our menu options have changed");
         System.out.println("1 - add account");
@@ -15,10 +25,65 @@ public class App {
         System.out.println("10 - exit program");
     }
 
+    /**
+     * Returns a new Account object
+     * <p>
+     * Prompts program user for info: name, account type, initial funds
+     * <p>
+     * Account constructor generates an accountNumber
+     * 
+     * @return Account
+     * @see Account
+     */
+    public static Account accountCreate() {
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("Provide info for the new account: ");
+        System.out.print("Account owner: ");
+        String owner = scnr.nextLine();
+        System.out.print("Account type: ");
+        String type = scnr.nextLine();
+        System.out.print("Initial funds: ");
+        Double money = scnr.nextDouble();
+        return new Account(owner, type, money);
+    }
+
+    /**
+     * Modifies an Account object
+     * Prompts program user for info: name, account type, initial funds
+     * <p>
+     * Usage:
+     * <p>
+     * Account newAccount = new Account(); //generates an account number only
+     * <p>
+     * accountCreate(newAccount);
+     * 
+     * @param a
+     */
+    public static void accountCreate(Account a) {
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("Provide info for the new account: ");
+        System.out.print("Account owner: ");
+        String owner = scnr.nextLine();
+        System.out.print("Account type: ");
+        String type = scnr.nextLine();
+        System.out.print("Initial funds: ");
+        Double money = scnr.nextDouble();
+        a.setAccountOwner(owner);
+        a.setAccountType(type);
+        a.setAccountFunds(money);
+    }
+
+    /**
+     * Driver for Banking application
+     * 
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         Scanner scnr = new Scanner(System.in);
         int userchoice = 0;
         boolean keepprompting = true;
+        ArrayList<Account> accounts = new ArrayList<>();
 
         do {
             printMenu();
@@ -35,37 +100,50 @@ public class App {
             }
             switch (userchoice) {
                 case 1:
-                    System.out.println("Under Construction");
+                    // accountCreate creates a NEW Account object
+                    // returns a reference to the Account object to
+                    // store in ArrayList accounts
+                    accounts.add(accountCreate());
                     break;
                 case 2:
+                    // TODO: account details by username
                     System.out.println("Under Construction");
                     break;
                 case 3:
+                    // TODO: account details by account number
                     System.out.println("Under Construction");
                     break;
                 case 4:
+                    // TODO: deposit to account number
                     System.out.println("Under Construction");
                     break;
                 case 5:
+                    // TODO: withdraw from account number
                     System.out.println("Under Construction");
                     break;
                 case 6:
+                    // TODO: transfer between accounts
                     System.out.println("Under Construction");
                     break;
                 case 7:
+                    // TODO: save accounts to file
                     System.out.println("Under Construction");
                     break;
                 case 8:
+                    // TODO: read accounts from file
                     System.out.println("Under Construction");
                     break;
                 case 9:
-                    System.out.println("Under Construction");
+                    // TODO: SORT accounts, then print accounts
+                    // TODO: why does this work?
+                    System.out.println(accounts.toString());
                     break;
                 case 10:
-                    System.out.println("Under Construction");
+                    // TODO: prompt to save before exit?
+                    System.exit(0);
                     break;
                 default:
-                    System.out.println(userchoice + "is not an option");
+                    System.out.println(userchoice + " is not an option");
                     break;
             }
 
