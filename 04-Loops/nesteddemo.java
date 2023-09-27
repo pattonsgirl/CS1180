4f820d1633371deb7bb567febf48898ad1bdd885
pattonsgirl <pattonsgirl@gmail.com>
2023-09-27 19:26:44 -0400

    nested demos breakout

--- 04-Loops/nesteddemo.java
+++ 04-Loops/nesteddemo.java
@@ -1,18 +1,46 @@
 public class nesteddemo {
     public static void main(String[] args){
-        // outer loop is "row"
+        // standard intro
+        for (int i = 0; i < 5; i++) {
+            System.out.println("i is: " + i);
+            for (int j = 0; j < 5; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        // prints a cube
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        // prints a triangle
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+        System.out.println();
+
+        // prints an upside down triangle
+        for (int i = 0; i < 5; i++) {
+            for (int j = i; j < 5; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Prints a triangle of characters
         char letter = 'A'; // = 65;
-        System.out.println("Character is" + letter);
+        System.out.println("Character is " + letter);
         for (int i = 0; i < 5; i++) {
             letter = 65; // or letter = 'A'
-            //System.out.println("i is: " + i);
             // inner loop is columns
             for (int j = 0; j <= i; j++) {
-                //System.out.print(j + " ");
-                //System.out.print("* ");
                 System.out.print(letter + " ");
-                //int userInput = (int)Math.pow((double)90, (double)2);
-                //letter += userInput;
                 letter += 1;
             }
             System.out.println();
